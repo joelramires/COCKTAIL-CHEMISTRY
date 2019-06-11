4fc08da9fd44927c110df4f61f49d338dede4cbf
Joel Ramirez <joelramirez@MacBook-Pro-5.home>
2019-06-11 06:12:42 -0400

    saved drinks update

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {getUserProfile} from '../utils/api'
+import { getUserProfile } from '../utils/api'
 
 class UserProfile extends React.Component {
   state = {
-    userProfile: []
+    userProfile: {},
   }
 
 
@@ -24,18 +24,46 @@ class UserProfile extends React.Component {
   }
 
   render() {
-    return(
-      <div className = "col-12-md-4" >
-          <div className="p-2 rounded">
-            <h3 id="full-name" className="font-weight-bold">{this.state.userProfile.firstName} 
+    return (
+      <div className="col-12-md-4" >
+        <div className="p-2 rounded">
+          <h3 id="full-name" className="font-weight-bold">{this.state.userProfile.firstName} 
             {this.state.userProfile.lastName}</h3>
-            <div className="card-body">
-              <button id="logout" className="btn btn-block btn-danger" onClick={this.handleLogOut} > logout</button>
+          <div className="card-body">
+            <button id="logout" className="btn btn-block btn-danger" onClick={this.handleLogOut} > logout</button>
+            <div className="mt-3 row">
+              {Object.keys(this.state.userProfile).length ? (this.state.userProfile.savedDrinks.map(drink => {
+                console.log(drink);
+                return (
+                  <div className="col-12 col-md-6" >
+                    <div className="card">
+                      <img class="card-img-top" src={drink.image} alt="Card image cap" />
+                      <div className="card-body">
+                        <h2>Name: {drink.name}</h2>
+                        Glass Type: {drink.glassType}<br />
+                        Instructions: {drink.instructions}<br /><br />
+                        Ingredients:<br />
+                        <ul className="list-group list-group-flush">
+                          {drink.ingredients.map((ingredient, index) => {
+                            return (
+                              <li key={ingredient} className="list-group-item">{drink.measurements[index] ? `${drink.measurements[index]} - ` : ""}{ingredient}</li>
+                            )
+                          })}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                )})
+              ) : ""}
+
             </div>
+
           </div>
+        </div>
       </div>
     )
   }
 }
+
 
 export default UserProfile;
